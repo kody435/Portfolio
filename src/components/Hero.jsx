dae26c316812b57dfa0c3bce0667d9f75322f2f5
Cody <bittusmaths@gmail.com>
2022-09-05 21:54:46 +0530

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,20 @@ const Hero = () => {
   return (
       <div>
           <div>
-              <section className="flex items-center flex-col p-12 pt-8">
-                  <h1 className="text-8xl font-black pb-6">Hey There! 👋</h1>
-                  <p className="text-4xl font-semibold">
+              <section className="flex items-center flex-col p-5 pt-8">
+                  <h1 className="text-5xl md:text-7xl lg:text-8xl font-black pb-6">Hey There! 👋</h1>
+                  <p className="text-3xl font-semibold text-center">
                       Welcome to Param's Digital home 🏡
                   </p>
               </section>
-              <section className="flex items-center flex-col mx-64 pt-12">
-                  <p className="text-3xl font-semibold flex-row text-center">
+              <section className="flex items-center flex-col mx-10 md:mx-32 lg:mx-64 pt-12">
+                  <p className="text-xl  lg:text-3xl font-semibold flex-row text-center">
                       Param is a Frontend Engineer currently building{" "}
-                      <a href="https://snarkyidiot.live/" target="_blank" rel="noreferrer" className=' font-black'><abbr title="Click to visit">OCTULUS</abbr></a>
+                      <a href="https://snarkyidiot.live/" target="_blank" rel="noreferrer" className=' font-black'><abbr title="Click to visit"><bold>OCTULUS</bold></abbr></a>
                       . He loves creating awesome stuff and writing things he learns along
                       the way, usually about JavaScript, React, and Python.
                   </p>
-                  <div className='pt-16'>
+                  <div className='pt-24 lg:pt-16'>
                         <box-icon type='solid' name='chevron-down' size="lg" animation="fade-down"></box-icon>
                   </div>
               </section>
